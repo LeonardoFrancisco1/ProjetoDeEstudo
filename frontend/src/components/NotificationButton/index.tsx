85a206dd28bab06d9cc77044c7604c223d4360a9
unknown <you@example.com>
2022-07-13 20:35:29 -0400

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,4 @@
-import icon from '../../assets/img/notification-icon.svg'
+import icon from '../../assets/img/notification-icon.svg';
 
 
 
@@ -21,4 +21,4 @@ function NotificationButton() {
 
 }
 
-export default NotificationButton
+export default NotificationButton;
